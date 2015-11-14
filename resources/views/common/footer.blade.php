93174a28c09a2b747fb7b2c2b35d61a20b85b567
rexon <rexondelosreyes88@gmail.com>
2015-11-14 12:25:14 +0800

    Ignoring something

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,11 +1,11 @@
 
-<script type="text/javascript" src="{!! asset('build/js/vendor.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/app.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/routes.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/materialInit.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/services/AjaxService.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/services/AuthService.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('js/controllers/AuthController.js') !!}"></script>
+<script type="text/javascript" src="{!! asset('build/js/all.min.js') !!}"></script>
+{{--<script type="text/javascript" src="{!! asset('js/app.js') !!}"></script>--}}
+{{--<script type="text/javascript" src="{!! asset('js/routes.js') !!}"></script>--}}
+{{--<script type="text/javascript" src="{!! asset('js/materialInit.js') !!}"></script>--}}
+{{--<script type="text/javascript" src="{!! asset('js/services/AjaxService.js') !!}"></script>--}}
+{{--<script type="text/javascript" src="{!! asset('js/services/AuthService.js') !!}"></script>--}}
+{{--<script type="text/javascript" src="{!! asset('js/controllers/AuthController.js') !!}"></script>--}}
 
 </body>
 </html>
