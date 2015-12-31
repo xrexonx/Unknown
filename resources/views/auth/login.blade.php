76a57ba711fb025c936cee1015df97d483806ac2
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2015-12-31 10:30:13 -0800

    Modified login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,33 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-    <div class="center-vertically row">
+    <div class="container center-vertically">
         <form method="POST" action="/auth/login">
 
             {{ csrf_field() }}
 
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}">
                     <label for="email">Email</label>
                 </div>
             </div>
+
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="password" type="password" class="validate" name="password">
                     <label for="password">Password</label>
                 </div>
             </div>
 
-            <div class="group right-align">
-                <button class="btn waves-effect waves-light teal lighten-2" type="submit">Log me In
-                    <i class="material-icons right">send</i>
-                </button>
+            <div class="row">
+                <div class="input-field col s12 m12 l4 offset-l4">
+                    <input type="submit" class="btn waves-effect waves-light teal lighten-2" value="Log me In">
+                </div>
             </div>
-
+            
         </form>
     </div>
-</div>
 
 @endsection
 
