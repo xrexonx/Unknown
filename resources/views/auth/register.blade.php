bb3ff43f45a3e157ec3ce1e4f8ce8fe9edf09e5e
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2015-12-31 10:35:10 -0800

    Modified signup form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,39 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-    <div class="row center-vertically">
+    <div class="container center-vertically">
         <form method="POST" action="/auth/register">
             {{ csrf_field() }}
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="name" type="text" class="validate" name="name" value="{{ old('name') }}">
                     <label for="name">Name</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}">
                     <label for="email">Email</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="password" type="password" class="validate" name="password">
                     <label for="password">Password</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s12 m12 l4 offset-l4">
                     <input id="password_confirmation" type="password" class="validate" name="password_confirmation">
                     <label for="password_confirmation">Confirm Password</label>
                 </div>
             </div>
-            <div class="group right-align">
-                <button class="btn waves-effect waves-light teal lighten-2"  type="submit">Sign me up!
-                    <i class="material-icons right">send</i>
-                </button>
+
+            <div class="row">
+                <div class="input-field col s12 m12 l4 offset-l4">
+                    <input type="submit" class="btn waves-effect waves-light teal lighten-2" value="Sign me up">
+                </div>
             </div>
         </form>
     </div>
-</div>
 @endsection
